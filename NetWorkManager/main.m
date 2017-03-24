150b00e5456cb1a57945a63689fad51be6426b76
liwei <fanxiaobin@liweideMacBook-Air.local>
2017-03-24 10:21:16 +0800

    Initial Commit

---
+++ NetWorkManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetWorkManager
+//
+//  Created by fanxiaobin on 2017/3/24.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
